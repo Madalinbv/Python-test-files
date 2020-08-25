9c3529168c7e4a3903706f4d96c3840a7e5f22c4
madalinrauch <email@test.com>
2020-08-25 06:52:27 +0300

    Added Day1 solution from Cloud 99

--- Real_project/emissions.py
+++ Real_project/emissions.py
@@ -1,11 +1,11 @@
 import csv
 
 with open("/Users/madalinrauch/IdeaProjects/Python-test-files/Real_project/Emissions.csv", "rt") as file_read:
-    data=csv.reader(file_read)
+    data = csv.reader(file_read)
     result = {}
     for raw in data:
         key = raw[0]
-        result[key] = list(raw [1:])
+        result[key] = list(raw[1:])
 for item in result:
     print(item + " - " + str(result[item]))
 file_read.close()
---
+++ Twillio-API/credentials.py
@@ -0,0 +1,4 @@
+account_sid = ""
+auth_token = ""
+my_cell = ""
+my_twilio = ""
